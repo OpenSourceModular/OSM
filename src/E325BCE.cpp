8ab1dc1ce7664094da6a10919d412d5b5f99bbf9
Justin Ahrens <justin@ahrens.net>
2023-05-25 14:10:40 -0600

    Cleaning up code

--- src/E325BCE.cpp
+++ src/E325BCE.cpp
@@ -3,10 +3,12 @@
 #define TRIG_TIME 1e-3f
 #define DEBUG_PRINT 1
 
+#define NORMAL 1
+#define STEPS_16 1
 struct E325BCE : Module {
 	enum ParamId {
 		STEPS_PARAM,
-		NUMSTEPS_PARAM,
+		NumberOfSteps_PARAM,
 		PULSES_PARAM,
 		MODE_PARAM,
 		OFFSET_PARAM,
@@ -32,192 +34,187 @@ struct E325BCE : Module {
 		MAINOUTLED_LIGHT,
 		LIGHTS_LEN
 	};
-	dsp::PulseGenerator pgen;
-	dsp::PulseGenerator auxPgen;
+	dsp::PulseGenerator PulseGenerator;
+	dsp::PulseGenerator AuxOutPulseGenerator;
 	dsp::SchmittTrigger clockDetector;
 	dsp::SchmittTrigger resetDetector;
-	int stepNr = 0;
-	int rhythm[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
-	int rhythm2[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
-    int rhythm3[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};	
-	int lastNumSteps = 0;
-	int numSteps = 16;
+	int StepNumber = 0;
+	int pattern1[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+	int pattern2[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+    int pattern3[32] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};	
+	int lastNumberOfSteps = 0;
+	int NumberOfSteps = 16;
 
 	E325BCE() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(STEPS_PARAM, 1.f, 16.f, 16.f, "Steps");
-		paramQuantities[STEPS_PARAM]->snapEnabled = true;
-		configParam(NUMSTEPS_PARAM, 0.f, 1.f, 0.f, "16/32");
-		configParam(PULSES_PARAM, 0.f, 16.f, 16.f, "Pulses");
-		paramQuantities[PULSES_PARAM]->snapEnabled = true;
-		configParam(MODE_PARAM, 0.f, 1.f, 0.f, "Norm/XOR!!!");
-		configParam(OFFSET_PARAM, 0.f, 16.f, 0.f, "Offset");
-		paramQuantities[OFFSET_PARAM]->snapEnabled = true;
-		configInput(CLOCK_INPUT, "Clock In");
-		configInput(RESET_INPUT, "Reset In");
-		configInput(STEPSCV_INPUT, "Steps CV");
-		configInput(PULSESCV_INPUT, "Pulses CV");
-		configInput(OFFSETCV_INPUT, "Offset CV");
-		configOutput(AUXOUT_OUTPUT, "Aux Pulse");
+		configParam (STEPS_PARAM,         1.f, 16.f, 16.f, "Steps");
+		configParam (NumberOfSteps_PARAM, 0.f,  1.f,  0.f, "16/32");
+		configParam (PULSES_PARAM,        0.f, 16.f, 16.f, "Pulses");
+		configParam (MODE_PARAM,          0.f,  1.f,  1.f, "Norm/XOR!!!");
+		configParam (OFFSET_PARAM,        0.f, 16.f,  0.f, "Offset");
+		configInput (CLOCK_INPUT,    "Clock In");
+		configInput (RESET_INPUT,    "Reset In");
+		configInput (STEPSCV_INPUT,  "Steps CV");
+		configInput (PULSESCV_INPUT, "Pulses CV");
+		configInput (OFFSETCV_INPUT, "Offset CV");
+		configOutput(AUXOUT_OUTPUT,  "Aux Pulse");
 		configOutput(MAINOUT_OUTPUT, "Pulse");
+		paramQuantities[STEPS_PARAM] -> snapEnabled = true;
+		paramQuantities[PULSES_PARAM]-> snapEnabled = true;
+		paramQuantities[OFFSET_PARAM]-> snapEnabled = true;
 	}
 
 	void process(const ProcessArgs& args) override;
-	void euclid(int steps, int pulses){
-		
-		for (int y = 0; y<steps; y++) rhythm[y] = 0;
-		int bucket = 0;
-		for (int a=0; a < steps; a++){
-		    bucket += pulses;
-		    if (bucket >= steps) {
-		      bucket -= steps;
-		      rhythm[a] = 1;
-		    } else rhythm[a] = 0;
-		}
-		int tempArray[32] = {0,0,0,0, 0,0,0,0 ,0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 ,0,0,0,0, 0,0,0,0};
-		tempArray[0] = rhythm[numSteps-1];
-		for(int x = 1; x<numSteps; x++) tempArray[x] = rhythm[x-1];
-		for(int y = 0; y<numSteps; y++) rhythm[y] = tempArray[y];
-	}
-	void euclid2(int steps, int pulses){
 
-		for (int y = 0; y<steps; y++) rhythm2[y] = 0;
+	void GenerateEuclidPattern(int *patternArray, int steps, int pulses){
+		
+		for (int i = 0; i<steps; i++) patternArray[i] = 0;
 		int bucket = 0;
-		for (int a=0; a < steps; a++){
-		    bucket += pulses;
+		for (int i=0; i < steps; i++){
+			bucket += pulses;
 		    if (bucket >= steps) {
-		      bucket -= steps;
-		      rhythm2[a] = 1;
-		    } else rhythm2[a] = 0;
+		    	bucket -= steps;
+		    	patternArray[i] = 1;
+		    } else patternArray[i] = 0;
 		}
 		int tempArray[32] = {0,0,0,0, 0,0,0,0 ,0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 ,0,0,0,0, 0,0,0,0};
-		tempArray[0] = rhythm2[numSteps-1];
-		for(int x = 1; x<numSteps; x++) tempArray[x] = rhythm2[x-1];
-		for(int y = 0; y <numSteps; y++) rhythm2[y] = tempArray[y];
-	}			
-			
+		tempArray[0] = pattern1[NumberOfSteps-1];
+		for(int i = 1; i<NumberOfSteps; i++) tempArray[i] = pattern1[i-1];
+		for(int i = 0; i<NumberOfSteps; i++) pattern1[i] = tempArray[i];
+	}				
 };
 
 void E325BCE::process(const ProcessArgs &args) {
-	if ((int)params[NUMSTEPS_PARAM].getValue() != lastNumSteps) {
-		lastNumSteps = (int)params[NUMSTEPS_PARAM].getValue();
-		if ((int)params[NUMSTEPS_PARAM].getValue() == 0) {
+
+	// Get the value of the Number Of Steps Switch
+	int NumberOfStepsSwitchValue = (int)params[NumberOfSteps_PARAM].getValue() ;
+	// Get the value of the Mode Switch
+	int ModeSwitchValue = (int)params[MODE_PARAM].getValue();
+	
+	// Get the steps knob amount and steps CV and add them up for the final value
+	// If the final value is more than the max, set it to the max
+	int StepsKnobValue = (int)params[STEPS_PARAM].getValue(); 
+	int StepsCVValue = (int)inputs[STEPSCV_INPUT].getVoltage();
+	int StepsValue = StepsKnobValue + StepsCVValue;
+	if (StepsValue > NumberOfSteps) StepsValue = NumberOfSteps;
+
+	// Get the pulses knob amount and pulses CV and add them up for the final value
+	// If the final value is more than the max, set it to the max
+	int PulsesKnobValue = (int)params[PULSES_PARAM].getValue();
+	int PulsesCVValue = (int)inputs[PULSESCV_INPUT].getVoltage();
+	int PulsesValue = PulsesKnobValue + PulsesCVValue;
+	if (PulsesValue > NumberOfSteps) PulsesValue = NumberOfSteps;
+	
+	// Get the offset knob amount and offset CV and add them up for the final value
+	// If the final value is more than the max, set it to the max
+	int OffsetKnobValue = (int)params[OFFSET_PARAM].getValue();
+	int OffsetCVValue = (int)inputs[OFFSETCV_INPUT].getVoltage();
+	int OffsetValue = OffsetKnobValue + OffsetCVValue;
+	if (OffsetValue > NumberOfSteps) OffsetValue = NumberOfSteps;
+
+	// if the 16/32 switch has changed, then adjust knob parameters
+	if (NumberOfStepsSwitchValue != lastNumberOfSteps) {
+		lastNumberOfSteps = NumberOfStepsSwitchValue;
+		if ((int)params[NumberOfSteps_PARAM].getValue() != STEPS_16) {
 			configParam(STEPS_PARAM, 1.f, 16.f, 16.f, "Steps");
 			configParam(PULSES_PARAM, 0.f, 16.f, 16.f, "Pulses");
 			configParam(OFFSET_PARAM, 0.f, 16.f, 0.f, "Offset");
-			numSteps = 16;
+			NumberOfSteps = 16;
 		}
 		else {
 			configParam(STEPS_PARAM, 1.f, 32.f, 32.f, "Steps");
 			configParam(PULSES_PARAM, 0.f, 32.f, 32.f, "Pulses");
 			configParam(OFFSET_PARAM, 0.f, 32.f, 0.f, "Offset");
-			numSteps = 32;
+			NumberOfSteps = 32;
 		}
 		paramQuantities[STEPS_PARAM]->snapEnabled = true;
 		paramQuantities[PULSES_PARAM]->snapEnabled = true;
 		paramQuantities[OFFSET_PARAM]->snapEnabled = true;	
 	}
-	
-	if ((int)params[MODE_PARAM].getValue() == 1) 
+
+	if (ModeSwitchValue == 1) 
 	{
-		euclid((int)(params[STEPS_PARAM].getValue()+(int)inputs[STEPSCV_INPUT].getVoltage()),(int)(params[PULSES_PARAM].getValue())+(int)inputs[PULSESCV_INPUT].getVoltage());
+		GenerateEuclidPattern ( pattern1, StepsValue, PulsesValue);
 	}
 	else
 	{
-		euclid(numSteps,(int)(params[PULSES_PARAM].getValue())+(int)inputs[PULSESCV_INPUT].getVoltage());
-		euclid2(numSteps,(int)(params[STEPS_PARAM].getValue())+(int)inputs[STEPSCV_INPUT].getVoltage());
-		for (int w = 0; w<numSteps; w++){
-			rhythm3[w] = (rhythm[w] ^ rhythm2[w]);
+		GenerateEuclidPattern (pattern1, NumberOfSteps, StepsValue);
+		GenerateEuclidPattern (pattern2, NumberOfSteps, PulsesValue);
+
+		for (int w = 0; w<NumberOfSteps; w++){
+			pattern3[w] = (pattern1[w] ^ pattern2[w]);
 		}			
 	}
 	 
-	if (resetDetector.process(inputs[RESET_INPUT].getVoltage())) stepNr = 0;
+	if (resetDetector.process(inputs[RESET_INPUT].getVoltage())) StepNumber = 0;
 
 	if (clockDetector.process(inputs[CLOCK_INPUT].getVoltage())) {
 
-			int offsetVal = stepNr + params[OFFSET_PARAM].getValue()+(int)inputs[OFFSETCV_INPUT].getVoltage();
-			if (offsetVal > numSteps-1) {
-				offsetVal = offsetVal - numSteps-1;
-			}
-			if (offsetVal > numSteps-1) {
-				offsetVal = offsetVal - numSteps-1;
-			}			
-			if ((int)params[MODE_PARAM].getValue() == 1) 
+			int offsetVal = StepNumber + OffsetValue;
+					
+			if (ModeSwitchValue == NORMAL) 
 			{
-				if (rhythm[offsetVal])
-					{
-					pgen.trigger(TRIG_TIME);
-					}
-				else 
-					{
-					auxPgen.trigger(TRIG_TIME);
-					}				
+				if (pattern1[offsetVal]) PulseGenerator.trigger(TRIG_TIME);
+				else               AuxOutPulseGenerator.trigger(TRIG_TIME);				
 			}
 			else
 			{
-				if (rhythm3[offsetVal])
-					{
-					pgen.trigger(TRIG_TIME);
-					}
-				else 
-					{
-					auxPgen.trigger(TRIG_TIME);
-					}
+				if (pattern3[offsetVal]) PulseGenerator.trigger(TRIG_TIME);
+				else               AuxOutPulseGenerator.trigger(TRIG_TIME);
 
 			}
-			stepNr = (stepNr + 1);
+			StepNumber++;
 
-			//if (stepNr > numSteps-1) stepNr = 0;
-			if ((int)params[MODE_PARAM].getValue() == 1) 
+			if (ModeSwitchValue == NORMAL) 
 			{
-				if (stepNr > ((int)(params[STEPS_PARAM].getValue())-1)) stepNr = 0;
+				if (StepNumber > StepsValue-1) StepNumber = 0;
 			}
 			else
 			{
-				if(stepNr > numSteps-1) stepNr=0;
+				if(StepNumber > NumberOfSteps-1) StepNumber=0;
 			}
 			
 			printf("=-=-=-=-=-=-=-=- =-=-\n");
 			if (DEBUG_PRINT) {
-			printf("Step: %d\n", stepNr);
-			if ((int)params[MODE_PARAM].getValue() == 1) 
+			printf("Step: %d\n", StepNumber);
+			if (ModeSwitchValue == NORMAL) 
 			{
 				for (int e = 0; e < (int)(params[STEPS_PARAM].getValue()); e++){
-					if (rhythm[e]) printf("1");
+					if (pattern1[e]) printf("1");
 					else printf("0");
 				}
 			} 
 			else
 			{
-				for (int e = 0; e < numSteps; e++){
-					if (rhythm[e]) printf("1");
+				for (int e = 0; e < NumberOfSteps; e++){
+					if (pattern1[e]) printf("1");
 					else printf("0");
 				}
 			}
 			printf(" \n");
-			for (int f = 0; f < numSteps; f++){
-				if (rhythm2[f]) printf("1");
+			for (int f = 0; f < NumberOfSteps; f++){
+				if (pattern2[f]) printf("1");
 				else printf("0");
 			}
 			printf(" \n");
-			for (int f = 0; f < numSteps; f++){
-				int offsetVal2 = f + params[OFFSET_PARAM].getValue()+(int)inputs[OFFSETCV_INPUT].getVoltage();
-				if (offsetVal2 > numSteps-1) 
+			for (int f = 0; f < NumberOfSteps; f++){
+				int offsetVal2 = f + OffsetKnobValue+(int)inputs[OFFSETCV_INPUT].getVoltage();
+				if (offsetVal2 > NumberOfSteps-1) 
 				{
-					offsetVal2 = offsetVal2 - numSteps-1;
+					offsetVal2 = offsetVal2 - NumberOfSteps-1;
 				}
-				if (offsetVal2 > numSteps-1) 
+				if (offsetVal2 > NumberOfSteps-1) 
 				{
-					offsetVal2 = offsetVal2 - numSteps-1;
+					offsetVal2 = offsetVal2 - NumberOfSteps-1;
 				}
-				if (rhythm3[offsetVal2]) printf("1");
+				if (pattern3[offsetVal2]) printf("1");
 				else printf("0");
 			}
 			printf(" \n");
 			}
 	}
-	bool pulse = pgen.process( args.sampleTime*2 );
-	bool apulse = auxPgen.process( args.sampleTime*2 );
+	bool pulse = PulseGenerator.process( args.sampleTime );
+	bool apulse = AuxOutPulseGenerator.process( args.sampleTime );
 	outputs[MAINOUT_OUTPUT].setVoltage(pulse ? 10.f : 0.f);
 	outputs[AUXOUT_OUTPUT].setVoltage(apulse ? 10.f : 0.f);
 	lights[CLOCKLED_LIGHT].setSmoothBrightness(inputs[CLOCK_INPUT].getVoltage(),args.sampleTime);
@@ -236,7 +233,7 @@ struct E325BCEWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(15.515, 27.651)), module, E325BCE::STEPS_PARAM));
-		addParam(createParam<CKSS>(mm2px(Vec(23.085, 31.643)), module, E325BCE::NUMSTEPS_PARAM));
+		addParam(createParam<CKSS>(mm2px(Vec(23.085, 31.643)), module, E325BCE::NumberOfSteps_PARAM));
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(15.337, 47.728)), module, E325BCE::PULSES_PARAM));
 		addParam(createParam<CKSS>(mm2px(Vec(23.264, 51.948)), module, E325BCE::MODE_PARAM));
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(15.383, 67.865)), module, E325BCE::OFFSET_PARAM));
